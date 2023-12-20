f9225076a85f538c324b1eed9e376f98f9098771
aman <bhateriyaaman@gmail.com>
2023-12-20 22:19:31 +0530

    create exception handling middleware, to handle exception globly/all around the application.

---
+++ EMS.Api/Common/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Net;
+using System.Reflection;
+
+namespace EMS.Api.Common
+{   
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var statusCode = ex switch
+                {   
+                    NullReferenceException => HttpStatusCode.BadRequest,    
+                     _ => HttpStatusCode.InternalServerError
+                };
+
+                string? innerExcetion = null;
+                if (ex.InnerException != null)
+                {
+                    innerExcetion = ex.InnerException.Message;
+                }
+
+                context.Response.StatusCode = (int)statusCode;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync($"{{\"message\": \"Internal Server Error\"," +
+                                                    $"\"error\": \"{ex.Message}\"," +
+                                                    $"\"innerExcetion\": \"{innerExcetion}\"}} ");
+
+
+            }
+        }
+    }
+}
