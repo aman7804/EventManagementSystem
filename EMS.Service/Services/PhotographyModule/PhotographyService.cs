ed9c986bac8bee129b4395552d23a58e2175f08c
aman <bhateriyaaman@gmail.com>
2023-12-27 16:16:11 +0530

    create photographyDTO, create map for photographyDTO to photographyEntity.
    create photographyService and photographyService. register service in startupConfigration.

---
+++ EMS.Service/Services/PhotographyModule/PhotographyService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using EMS.Entity.Entity;
+using EMS.Repository.Repository.PhotographyModule;
+using EMS.Service.Base;
+using EMS.Service.DTO;
+
+namespace EMS.Service.Services.PhotographyModule
+{
+    public class PhotographyService : BaseService<PhotographyEntity, PhotographyDTO>, IPhotographyService
+    {
+        public PhotographyService(IMapper mapper, IPhotographyRepository photographyRepository) : base(mapper, photographyRepository) { }
+    }
+}
