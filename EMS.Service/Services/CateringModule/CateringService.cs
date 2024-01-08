556ec8606c8fc3fcacb17c6888aeba53ff2bbdff
aman <bhateriyaaman@gmail.com>
2024-01-08 20:08:42 +0530

    handled currectUser problem roughly for now to work on bookingModule. create bookingEntity and register it in dbContext. add-migration "booking".

--- EMS.Service/Services/CateringModule/CateringService.cs
+++ EMS.Service/Services/CateringModule/CateringService.cs
@@ -3,11 +3,12 @@ using EMS.Entity.Entity;
 using EMS.Repository.Repository.CateringModule;
 using EMS.Service.Base;
 using EMS.Service.DTO;
+using Microsoft.AspNetCore.Http;
 
 namespace EMS.Service.Services.CateringModule
 {
     public class CateringService : BaseService<CateringEntity, CateringDTO>, ICateringService
     {
-        public CateringService(IMapper mapper, ICateringRepository cateringRepository) : base(mapper, cateringRepository) { }   
+        public CateringService(IMapper mapper, ICateringRepository cateringRepository, IHttpContextAccessor contextAccessor) : base(mapper, cateringRepository, contextAccessor) { }   
     }
 }
