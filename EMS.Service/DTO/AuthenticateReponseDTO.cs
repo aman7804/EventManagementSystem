4bff9bd9b632439d14849836f3ae2b0db0a773ae
aman <bhateriyaaman@gmail.com>
2024-01-15 16:46:49 +0530

    reduce user-data to be sent on authentication.

--- EMS.Service/DTO/AuthenticateReponseDTO.cs
+++ EMS.Service/DTO/AuthenticateReponseDTO.cs
@@ -1,22 +1,15 @@
-﻿using EMS.Entity;
-using EMS.Shared;
-
-namespace EMS.Service.DTO;
+﻿namespace EMS.Service.DTO;
 public class AuthenticateResponseDTO : BaseDTO
 {
-    public string FirstName { get; set; } = string.Empty!;
-    public string LastName { get; set; } = string.Empty!;
+    public string FullName { get; set; } = string.Empty!;
     public string EmailId { get; set; } = string.Empty!;
-    public string MobileNo { get; set; } = string.Empty!;
     public string Token { get; set; }
 
     public AuthenticateResponseDTO(UserDTO user, string token)
     {
         Id = user.Id;
-        FirstName = user.FirstName;
-        LastName = user.LastName;
+        FullName = string.Join(" ", user.FirstName, user.LastName);
         EmailId = user.EmailId;
-        MobileNo = user.MobileNo;
         Token = token;
     }
 }
