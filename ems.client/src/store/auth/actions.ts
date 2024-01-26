0c5d1bd0360a813d2f21396cf998104780f0f232
aman <bhateriyaaman@gmail.com>
2024-01-26 23:26:20 +0530

    create actionMethods for Sigup-module

--- ems.client/src/store/auth/actions.ts
+++ ems.client/src/store/auth/actions.ts
@@ -2,6 +2,9 @@ import {
     LOGIN_REQUEST,
     LOGIN_FAILURE,
     LOGIN_SUCCESS,
+    SIGNUP_REQUEST,
+    SIGNUP_SUCCESS,
+    SIGNUP_FAILURE,
   } from "./action.types";
   
   import {
@@ -11,19 +14,36 @@ import {
     LoginSuccessPayload,
     LoginFailure,
     LoginFailurePayload,
+    SignupRequest,
+    SignupPayload,
+    SignupSuccessPayload,
+    SignupSuccess,
+    SignupFailurePayload,
+    SignupFailure,
   } from "./types";
   
   export const loginRequest = (payload: LoginPayload): LoginRequest => ({
     type: LOGIN_REQUEST,
     payload,
   });
-  
   export const loginSuccess = (payload: LoginSuccessPayload): LoginSuccess => ({
     type: LOGIN_SUCCESS,
     payload,
   });
-  
   export const loginFailure = (payload: LoginFailurePayload): LoginFailure => ({
     type: LOGIN_FAILURE,
     payload,
   });
+
+  export const signupRequest = (payload: SignupPayload): SignupRequest => ({
+    type: SIGNUP_REQUEST,
+    payload
+  })
+  export const signupSuccess = (payload: SignupSuccessPayload): SignupSuccess => ({
+    type: SIGNUP_SUCCESS,
+    payload
+  })
+  export const signupFailure = (payload: SignupFailurePayload): SignupFailure => ({
+    type: SIGNUP_FAILURE,
+    payload
+  })
