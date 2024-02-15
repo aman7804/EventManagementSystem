b2b37e96f854380ca24f2a62b9b218522bd7eccb
aman <bhateriyaaman@gmail.com>
2024-02-16 04:53:43 +0530

    user - save, get, delete apis

---
+++ EMS.Service/DTO/SaveUserDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace EMS.Service.DTO
+{
+    public class SaveUserDTO : BaseDTO
+    {
+        public string FirstName { get; set; } = string.Empty!;
+        public string LastName { get; set; } = string.Empty!;
+        public string EmailId { get; set; } = string.Empty!;
+        public string MobileNo { get; set; } = string.Empty!;
+        public string Password { get; set; } = string.Empty!;
+        public string Address { get; set; } = string.Empty!;
+        public int CityId { get; set; }
+
+    }
+}
