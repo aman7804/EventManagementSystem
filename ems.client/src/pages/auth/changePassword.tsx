7a915a3a75221b231384c323dbf7f3d3a7ad695a
aman <bhateriyaaman@gmail.com>
2024-02-18 14:00:09 +0530

    remove design related to auth module and center change-password content

--- ems.client/src/pages/auth/changePassword.tsx
+++ ems.client/src/pages/auth/changePassword.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardActions,
@@ -12,12 +13,10 @@ import {
   OutlinedInput,
   Typography,
 } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import * as images from "../../assets/images";
 import { IChangePassword, IChangePasswordContainerDispatch } from "../../interfaces/auth.interface";
 import { useNavigate } from "react-router";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import { ChangePasswordPayload, LoginSuccessPayload } from "store/auth/types";
 import { toast } from "react-toastify";
 import { PASSWORD_PATTERN } from "utils/constants";
@@ -52,15 +51,6 @@ export const ChangePasswordForm = (props: ChangePasswordProps) => {
     reset,
     formState: { errors },
   } = useForm<IChangePassword>();
-
-  useEffect(() => {
-    AOS.init({
-      easing: "ease-in",
-      duration: 400,
-      once: true,
-    });
-    AOS.refresh();
-  }, []);
 
 const maxPasswordLength = 16
   const getErrorMessage = (fieldName: string, type: string|undefined): string => {
@@ -121,183 +111,172 @@ const maxPasswordLength = 16
   console.log(errors)
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="login-wrapper">
-        <Grid container className="login-container">
-          <Grid xs={12} sm="auto">
-            <a
-              data-aos="fade-right"
-              data-aos-delay="300"
-              href="/"
-              title="Event Management System"
-              className="login-logo"
-            >
-              <img src={images.loginLogo} alt="Event Management System" />
-            </a>
-          </Grid>
-          <Grid xs={12} sm="auto">
-            <Card
-              className="login-card"
-              data-aos="fade-left"
-              data-aos-delay="300"
-            >
-              <CardContent>
-                <Typography variant="h2" className="card-heading">
-                  ChangePassword
-                </Typography>
-                <div className="login-content-form">
-                  <Grid container rowSpacing={0.1} columnSpacing={2}>
-                    <Grid item xs={12}>
-                      <FormControl
-                        variant="outlined"
-                        fullWidth
-                      >
-                        <InputLabel
-                          htmlFor="password"
-                          error={!!errors.oldPassword}
-                        >
-                          Old Password <span className="color-red">*</span>
-                        </InputLabel>
-                        <OutlinedInput
-                          id="oldPassword"
-                          className="with-icon"
-                          type={showOldPassword ? "text" : "password"}
-                          endAdornment={
-                            <InputAdornment position="end">
-                              <IconButton
-                                onClick={handleClickShowOldPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                                disableFocusRipple
-                                disableRipple
-                              >
-                                {showOldPassword ? (
-                                  <img
-                                    src={images.eyeOpen}
-                                    alt="show"
-                                  />
-                                ) : (
-                                  <img
-                                    src={images.eyeClose}
-                                    alt="hide"
-                                  />
-                                )}
-                              </IconButton>
-                            </InputAdornment>
-                          }
-                          label="Password"
-                          error={!!errors.oldPassword}
-                          {...register("oldPassword", {
-                            required: true,
-                            maxLength: maxPasswordLength,
-                            pattern: PASSWORD_PATTERN,
-                          })}
-                        />
-                        {!!errors.oldPassword && (
-                          <FormHelperText error>
-                            {getError("oldPassword")}
-                          </FormHelperText>
-                        )}
-                      </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <FormControl
-                        variant="outlined"  
-                        fullWidth
-                      >
-                        <InputLabel
-                          htmlFor="password"
-                          error={!!errors.newPassword}
-                        >
-                          New Password <span className="color-red">*</span>
-                        </InputLabel>
-                        <OutlinedInput
-                          id="newPassword"
-                          className="with-icon"
-                          type={showNewPassword ? "text" : "password"}
-                          endAdornment={
-                            <InputAdornment position="end">
-                              <IconButton
-                                onClick={handleClickShowNewPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                                disableFocusRipple
-                                disableRipple
-                              >
-                                {showNewPassword ? (
-                                  <img
-                                    src={images.eyeOpen}
-                                    alt="show"
-                                  />
-                                ) : (
-                                  <img
-                                    src={images.eyeClose}
-                                    alt="hide"
-                                  />
-                                )}
-                              </IconButton>
-                            </InputAdornment>
-                          }
-                          label="Password"
-                          error={!!errors.newPassword}
-                          {...register("newPassword", {
-                            required: true,
-                            maxLength: maxPasswordLength,
-                            pattern: PASSWORD_PATTERN,
-                            validate:(value)=>
-                              value === getValues("oldPassword")
-                                ? false : true
-                          })}
-                        />
-                        {!!errors.newPassword && (
-                          <FormHelperText error>
-                            {getError("newPassword")}
-                          </FormHelperText>
-                        )}
-                      </FormControl>
-                      <Typography color={()=>{
-                        const isNewPassValid = errors?.newPassword?.type === "pattern";
-                        const isOldPassValid = errors?.oldPassword?.type === "pattern";
-                        return isNewPassValid || isOldPassValid
-                        ? "red" : "gray"
-                      }
-                      }
-                      m={2}
-                      >
-                        Password must be 8+ characters with a special character,
-                        number, and uppercase letter.
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </div>
-              </CardContent>
-              <CardActions>
-              <Grid container spacing={2}>
-                <Grid item>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  title="Login"
-                  className="login-btn"
-                  disableElevation
-                >
-                  Change Password
-                </Button>
-                </Grid>
-                <Grid item xl={6} md={6} xs={6}>
-                  <Button
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+      <Box maxWidth={500}>
+        <Card>
+          <CardContent>
+            <Typography variant="h2" className="card-heading" mb={2}>
+              ChangePassword
+            </Typography>
+            <div>
+              <Grid container rowSpacing={0.1} columnSpacing={2}>
+                <Grid item xs={12}>
+                  <FormControl
                     variant="outlined"
-                    className="btn-cancel"
-                    onClick={onModalClose}
+                    fullWidth
+                  >
+                    <InputLabel
+                      htmlFor="password"
+                      error={!!errors.oldPassword}
                     >
-                    Cancel
-                  </Button>
+                      Old Password <span className="color-red">*</span>
+                    </InputLabel>
+                    <OutlinedInput
+                      id="oldPassword"
+                      className="with-icon"
+                      type={showOldPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handleClickShowOldPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            disableFocusRipple
+                            disableRipple
+                          >
+                            {showOldPassword ? (
+                              <img
+                                src={images.eyeOpen}
+                                alt="show"
+                              />
+                            ) : (
+                              <img
+                                src={images.eyeClose}
+                                alt="hide"
+                              />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Password"
+                      error={!!errors.oldPassword}
+                      {...register("oldPassword", {
+                        required: true,
+                        maxLength: maxPasswordLength,
+                        pattern: PASSWORD_PATTERN,
+                      })}
+                    />
+                    {!!errors.oldPassword && (
+                      <FormHelperText error>
+                        {getError("oldPassword")}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl
+                    variant="outlined"  
+                    fullWidth
+                  >
+                    <InputLabel
+                      htmlFor="password"
+                      error={!!errors.newPassword}
+                    >
+                      New Password <span className="color-red">*</span>
+                    </InputLabel>
+                    <OutlinedInput
+                      id="newPassword"
+                      className="with-icon"
+                      type={showNewPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handleClickShowNewPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            disableFocusRipple
+                            disableRipple
+                          >
+                            {showNewPassword ? (
+                              <img
+                                src={images.eyeOpen}
+                                alt="show"
+                              />
+                            ) : (
+                              <img
+                                src={images.eyeClose}
+                                alt="hide"
+                              />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Password"
+                      error={!!errors.newPassword}
+                      {...register("newPassword", {
+                        required: true,
+                        maxLength: maxPasswordLength,
+                        pattern: PASSWORD_PATTERN,
+                        validate:(value)=>
+                          value === getValues("oldPassword")
+                            ? false : true
+                      })}
+                    />
+                    {!!errors.newPassword && (
+                      <FormHelperText error>
+                        {getError("newPassword")}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                  <Typography color={()=>{
+                    const isNewPassValid = errors?.newPassword?.type === "pattern";
+                    const isOldPassValid = errors?.oldPassword?.type === "pattern";
+                    return isNewPassValid || isOldPassValid
+                    ? "red" : "gray"
+                  }
+                  }
+                  m={2}
+                  >
+                    Password must be 8+ characters with a special character,
+                    number, and uppercase letter.
+                  </Typography>
                 </Grid>
               </Grid>
-              </CardActions>
-            </Card>
+            </div>
+          </CardContent>
+          <CardActions>
+          <Grid container spacing={2}>
+            <Grid item>
+            <Button
+              type="submit"
+              variant="contained"
+              title="Login"
+              className="login-btn"
+              disableElevation
+            >
+              Change Password
+            </Button>
+            </Grid>
+            <Grid item xl={6} md={6} xs={6}>
+              <Button
+                variant="outlined"
+                className="btn-cancel"
+                onClick={onModalClose}
+                >
+                Cancel
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
+          </CardActions>
+        </Card>
+      </Box>
+      </Grid>
     </form>
   );
 };
